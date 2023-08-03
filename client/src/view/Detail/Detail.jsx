4c328499fe526497e4e201891d7229022d69b079
apardov <and.pardo.vergara@gmail.com>
2023-08-02 20:19:38 -0400

    Se modifica card por product, Cards por ProductsContainer se generan codigo requerido para mostrar Card en Card Container y se agrega un detail al hacer click en cada Card, adicional se agrega bootstrap

---
+++ client/src/view/Detail/Detail.jsx
@@ -0,0 +1,20 @@
+//import { useParams } from 'react-router-dom';
+
+const Detail = () => {
+    return (
+        <div className="container text-center">
+            <div className="card col-md-12">
+                <h1>Detail</h1>
+                <img src="" alt="" />
+                <p>Name</p>
+                <p>Width</p>
+                <p>Weight</p>
+                <p>Description</p>
+                <p>Price</p>
+            </div>
+            
+        </div>
+    );
+}
+
+export default Detail;
